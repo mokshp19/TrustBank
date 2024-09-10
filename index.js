d009e769b4944a2f17f7f5b0249d7894cf162600
Vincent Tseng <174834371+VincentTsengCA@users.noreply.github.com>
2024-09-10 13:17:41 -0400

    Login section updated

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
+const passwordInput = document.getElementById("Password");
 
 
 function login() {
-    
+    window.scrollTo(0, 650);
 }
 
 function signUp() {
-    
+    window.scrollTo(0, 650);
+}
+
+function revealPassword() {
+    passwordInput.type = "text";
 }
