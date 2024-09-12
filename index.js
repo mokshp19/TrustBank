67acb5d881e623f75b91352babd25d6d32b30ec7
Vincent Tseng <174834371+VincentTsengCA@users.noreply.github.com>
2024-09-12 17:21:43 -0400

    Added login scroll animation

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+// index.js
 
 const passwordInput = document.getElementById("Password");
+const loginScrollDestinationY = 650;
 
+
+function scrollToLogin() {
+    if (document.documentElement.scrollTop < 650) {
+        window.scrollTo(0, document.documentElement.scrollTop + 10);
+    }
+
+    else if (document.documentElement.scrollTop > 650) {
+        window.scrollTo(0, document.documentElement.scrollTop - 10);
+    }
+
+    if (document.documentElement.scrollTop != 650) {
+        setTimeout(login, 1)
+    }
+}
 
 function login() {
-    window.scrollTo(0, 650);
+    scrollToLogin();
 }
 
 function signUp() {
-    window.scrollTo(0, 650);
+    scrollToLogin();
 }
 
 function revealPassword() {
