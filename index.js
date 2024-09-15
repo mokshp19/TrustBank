f6b3514b737894ec98d5384da60009a9a4f06084
Moksh Patel <Mokshpatels@gmail.com>
2024-09-14 22:03:19 -0400

    Eyes Open and refix of scroll

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 
 const passwordInput = document.getElementById("Password");
 const loginScrollDestinationY = 650;
+const passwordEye = document.getElementById("PasswordEye"); 
 
 
 function scrollToLogin() {
-    if (document.documentElement.scrollTop < 650) {
-        window.scrollTo(0, document.documentElement.scrollTop + 10);
-    }
+    // if (document.documentElement.scrollTop <= 650) {
+    //     window.scrollTo(0, document.documentElement.scrollTop + 10);
+    // }
 
-    else if (document.documentElement.scrollTop > 650) {
-        window.scrollTo(0, document.documentElement.scrollTop - 10);
-    }
+    // else if (document.documentElement.scrollTop > 650) {
+    //     window.scrollTo(0, document.documentElement.scrollTop - 10);
+    // }
 
-    if (document.documentElement.scrollTop != 650) {
-        setTimeout(login, 1)
-    }
+    // if (document.documentElement.scrollTop != 650) {
+    //     setTimeout(login, 1)
+    // }
+    window.scrollTo(0,loginScrollDestinationY )
 }
 
 function login() {
@@ -29,19 +31,23 @@ function signUp() {
 function togglePasswordVisibility(visible = null) {
     if (visible == true) {
         passwordInput.type = "text";
+        passwordEye.src = "images/EyesOpen.png";
     }
 
     else if (visible == false) {
         passwordInput.type = "password";
+        passwordEye.src = "images/EyeClose.png";
     }
 
     else {
         if (passwordInput.type == "text") {
             passwordInput.type = "password";
+            passwordEye.src = "images/EyeClose.png";
         }
 
         else {
             passwordInput.type = "text";
+            passwordEye.src = "images/EyesOpen.png";
         }
     }
 }
